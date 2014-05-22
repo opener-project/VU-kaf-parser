b8095e2920749db1c319dee37f685f1fecf98b24
Yorick Peterse <yorickpeterse@olery.com>
2014-05-22 14:52:36 +0200

    Bumped the version to 1.1.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='VUKafParserPy',
-      version='1.0',
+      version='1.1',
       description = 'Library in python to parse kaf files',
       author = 'Ruben Izquierdo',
       author_email = 'r.izquierdobevia@vu.nl',
       packages = ['VUKafParserPy']
       )
-      
+
